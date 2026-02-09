e7239cff14363fa25170969b42afef273efbc7ca
CaiqueAlves <caiquealves858@gmail.com>
2026-02-09 20:37:23 -0300

    Pesquisando com filtros

--- src/main/java/io/github/caiquealves/libraryapi/repository/AutorRepository.java
+++ src/main/java/io/github/caiquealves/libraryapi/repository/AutorRepository.java
@@ -2,8 +2,12 @@ package io.github.caiquealves.libraryapi.repository;
 
 import io.github.caiquealves.libraryapi.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.UUID;
 
 public interface AutorRepository extends JpaRepository <Autor, UUID> {
-
+    List<Autor>findByNome(String nome);
+    List<Autor>findByNacionalidade(String nacionalidade);
+    List<Autor>findByNomeAndNacionalidade(String nome, String nacionalidade);
 }
--- src/main/java/io/github/caiquealves/libraryapi/service/AutorService.java
+++ src/main/java/io/github/caiquealves/libraryapi/service/AutorService.java
@@ -4,26 +4,39 @@ import io.github.caiquealves.libraryapi.model.Autor;
 import io.github.caiquealves.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class AutorService {
-    private final AutorRepository Repository;
+    private final AutorRepository repository;
 
     public AutorService(AutorRepository repository) {
-        Repository = repository;
+        this.repository = repository;
     }
 
     public Autor salvar(Autor autor) {
-        return Repository.save(autor);
+        return repository.save(autor);
     }
 
     public Optional<Autor> obterPorId(UUID id) {
-        return Repository.findById(id);
+        return repository.findById(id);
     }
 
     public void deletar(Autor autor) {
-        Repository.delete(autor);
+        repository.delete(autor);
+    }
+
+    public List <Autor> pesquisar(String nome, String nacionalidade){
+        if(nome != null && nacionalidade != null){
+            return repository.findByNomeAndNacionalidade(nome, nacionalidade);
+        } else if (nome != null) {
+            return repository.findByNome(nome);
+        }else if (nacionalidade != null){
+            return repository.findByNacionalidade(nacionalidade);
+        }
+        return repository.findAll();
+
     }
 }
