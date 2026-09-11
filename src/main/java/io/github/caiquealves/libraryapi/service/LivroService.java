67a63238fc65b975e226660b1ed833f2075a13c8
CaiqueAlves <caiquealves858@gmail.com>
2026-09-11 19:50:43 -0300

    Implementando as Specifications da entidade Livro

--- src/main/java/io/github/caiquealves/libraryapi/service/LivroService.java
+++ src/main/java/io/github/caiquealves/libraryapi/service/LivroService.java
@@ -1,12 +1,18 @@
 package io.github.caiquealves.libraryapi.service;
 
+import io.github.caiquealves.libraryapi.model.GeneroLivro;
 import io.github.caiquealves.libraryapi.model.Livro;
 import io.github.caiquealves.libraryapi.repository.LivroRepository;
+import io.github.caiquealves.libraryapi.repository.specs.LivroSpecs.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import static io.github.caiquealves.libraryapi.repository.specs.LivroSpecs.*;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +29,22 @@ public class LivroService {
 
     public void deletar(Livro livro){
         repository.delete(livro);
+    }
+
+    public List<Livro> pesquisa(String isbn,  String titulo, String nomeAutor, GeneroLivro genero, Integer anoPublicacao){
+
+        Specification<Livro> specs = Specification.where((root, query, cb) -> cb.conjunction());
+
+        if(isbn != null){
+            specs = specs.and(isbnEqual(isbn));
+        }
+        if(titulo != null){
+            specs = specs.and(tituloLike(titulo));
+        }
+        if(genero != null){
+            specs = specs.and(generoEqual(genero));
+        }
+
+        return repository.findAll(specs);
     }
 }
