ac4fd232ce6b67f6eaea8fd8245543b755b8cbd9
CaiqueAlves <caiquealves858@gmail.com>
2026-09-17 19:25:02 -0300

    Validação de ano da data da publicação

--- src/main/java/io/github/caiquealves/libraryapi/controller/common/GlobalExceptionHandler.java
+++ src/main/java/io/github/caiquealves/libraryapi/controller/common/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package io.github.caiquealves.libraryapi.controller.common;
 
 import io.github.caiquealves.libraryapi.controller.dto.ErroCampo;
 import io.github.caiquealves.libraryapi.controller.dto.ErroResposta;
+import io.github.caiquealves.libraryapi.exceptions.CampoInvalidoException;
 import io.github.caiquealves.libraryapi.exceptions.OperacaoNaoPermitidaException;
 import io.github.caiquealves.libraryapi.exceptions.RegistroDuplicadoException;
 import org.springframework.http.HttpStatus;
@@ -41,6 +42,15 @@ public class GlobalExceptionHandler {
         return ErroResposta.conflito(e.getMessage());
     }
 
+    @ExceptionHandler(CampoInvalidoException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ErroResposta handleCampoInvalidoException(CampoInvalidoException e){
+        return new ErroResposta(
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                "Erro de validação.",
+                List.of(new ErroCampo(e.getCampo(), e.getMessage())));
+    }
+    
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErroResposta handleErrosNaoTratados(RuntimeException e){
--- src/main/java/io/github/caiquealves/libraryapi/validador/LivroValidador.java
+++ src/main/java/io/github/caiquealves/libraryapi/validador/LivroValidador.java
@@ -1,5 +1,6 @@
 package io.github.caiquealves.libraryapi.validador;
 
+import io.github.caiquealves.libraryapi.exceptions.CampoInvalidoException;
 import io.github.caiquealves.libraryapi.exceptions.RegistroDuplicadoException;
 import io.github.caiquealves.libraryapi.model.Livro;
 import io.github.caiquealves.libraryapi.repository.LivroRepository;
@@ -11,12 +12,15 @@ import java.util.Optional;
 @Component
 @RequiredArgsConstructor
 public class LivroValidador {
-
+    private static final int ANO_EXIGENCIA_PRECO = 2020;
     private final LivroRepository repository;
 
     public void validar(Livro livro){
         if(existeLivroComIsbn(livro)){
             throw new RegistroDuplicadoException("ISBN ja cadastrado");
+        }
+        if(isPrecoObrigatorioNulo(livro)){
+            throw new CampoInvalidoException("preco", "Para livros com ano de publicação a partir de 2020 é obrigatorio informar o preço");
         }
     }
 
@@ -31,5 +35,9 @@ public class LivroValidador {
                     .map(Livro::getId)
                     .stream()
                     .anyMatch(id -> !id.equals(livro.getId()));
+    }
+
+    private boolean isPrecoObrigatorioNulo(Livro livro){
+        return livro.getPreco() == null && livro.getDataPublicacao().getYear() >= ANO_EXIGENCIA_PRECO;
     }
 }
